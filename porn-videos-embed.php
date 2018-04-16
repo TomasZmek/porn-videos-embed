bc4102d371f8317f01a4124bd96e6960ba0bd5aa
Tomáš Zmek <tomas@zmek.eu>
2018-04-16 23:17:55 +0200

    add tube8, vporn,gotporn

--- porn-videos-embed.php
+++ porn-videos-embed.php
@@ -4,7 +4,7 @@
  * Plugin Name: Porn videos embed
  * Plugin URI: https://perteus.cz/plugins/pornvideo-embed
  * Description: Simble plugin to add shortcode to post and embed video from porntube sites.
- * Version: 0.3
+ * Version: 0.4
  * Author: Tomáš Zmek @ perteus
  * Author URI: https://zmek.eu
  * License: GPL3
@@ -22,6 +22,9 @@ function pve_register_shortcodes(){
   add_shortcode('xhamster', 'pve_xhamster');
   add_shortcode('pornhub', 'pve_pornhub');
   add_shortcode('tnaflix', 'pve_tnaflix');
+  add_shortcode('tube8', 'pve_tube8');
+  add_shortcode('vporn', 'pve_vporn');
+  add_shortcode('gotporn', 'pve_gotporn');
 }
 
 /*
@@ -105,4 +108,63 @@ function pve_tnaflix($atts){
   $tnaflix ='
         <iframe src="https://player.tnaflix.com/video/'.$tnaflix_id.'" width="'.$width.'" height="'.$height.'" frameborder="0"></iframe>';
   return $tnaflix;
+}
+
+/*
+ * Shorcode for tube8.com
+ */
+
+function pve_tube8($atts){
+
+    extract(shortcode_atts(array(
+        "url" => 'https://',
+        "width" => "708",
+        "height" => "560"
+    ), $atts));
+
+    $tube8 ='
+       <iframe src="'.$url.'" frameborder="0" height="'.$height.'" width="'.$width.'" scrolling="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" name="t8_embed_video"></iframe>';
+    return $tube8;
+}
+
+
+/*
+ *  Shortcode for vporn
+ */
+function pve_vporn($atts){
+
+  extract(shortcode_atts(array(
+    "url" => 'https://',
+    "width" => "580",
+    "height" => "460"
+  ), $atts));
+
+  $str = $url;
+  $re = '/\/(\d+)\//';
+  preg_match_all($re, $str,$matches, PREG_OFFSET_CAPTURE);
+  $vporn_id = $matches[1][0][0];
+  $vporn ='
+        <iframe src="https://www.vporn.com/embed/'.$vporn_id.'/" width="'.$width.'" height="'.$height.'"></iframe>';
+  return $vporn;
+}
+
+
+/*
+ *  Shortcode for gotporn
+ */
+function pve_gotporn($atts){
+
+  extract(shortcode_atts(array(
+    "url" => 'https://',
+    "width" => "640",
+    "height" => "480"
+  ), $atts));
+
+  $str = $url;
+  $re = '/\/video-(\d+)/';
+  preg_match_all($re, $str,$matches, PREG_OFFSET_CAPTURE);
+  $gotporn_id = $matches[1][0][0];
+  $gotporn ='
+        <iframe width="'.$width.'" height="'.$height.'" src="https://www.gotporn.com/video/'.$gotporn_id.'/embedframe" frameborder="0" allowfullscreen></iframe>';
+  return $gotporn;
 }
